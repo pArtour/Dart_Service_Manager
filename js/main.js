7554306e3faa28699eb121bd2a0ae05439ad3233
pArtour <edvardi72@gmail.com>
2019-03-31 16:07:16 +0700

    features section completed, feedbacks sections to go

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 new WOW().init();
+
+$(document).ready(function(){
+  $('.service__header-wrapper').on('click', function(){
+    $(this).next('.service__content-wrapper').toggleClass('.service__content-wrapper-active').slideToggle(600);
+  });
+});
